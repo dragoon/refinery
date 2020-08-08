951c9ab458dc466d363902f1171304e2e7e80b7d
Roman Prokofyev <roman.prokofyev@gmail.com>
2020-08-08 18:30:58 +0200

    fix label initialization

--- refinery/lights/lifx.py
+++ refinery/lights/lifx.py
@@ -6,7 +6,8 @@ from lifxlan import LifxLAN, Light
 def get_light(label: str) -> Optional[Light]:
     lan = LifxLAN()
     lights = lan.get_lights()
-    lights = [l for l in lights if l.label == label]
+    # need to call get_label as otherwise it's not initialized
+    lights = [l for l in lights if l.get_label() and l.label == label]
     if len(lights) == 1:
         return lights[0]
     return None
